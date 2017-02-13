82fe1640497054cbc014b7214243ad5ecf434f4f
David Mariluch <davidmariluch@mail.weber.edu>
2017-02-13 08:16:16 -0700

    Added comments and extra fields to registration
    Added comments
    Added fname, lname, confirm password fields to registration
    Added HOME link on login
    Added script methods to remove and add text to login and registration
    fields depending on if they are in focus
    Removed login, index, and registrations views extending layout

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,7 @@ var router = express.Router();
 // router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
 // });
+
 /* GET login page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Login' });
