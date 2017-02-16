3108bbb9ca784bb8626a89e67e958e718faa52f6
Tim3Ds <Tim3Ds@gmail.com>
2017-02-16 11:10:21 -0700

    schema user unique

--- models/schema.js
+++ models/schema.js
@@ -2,7 +2,7 @@ var objectID = schema.objectID;
 var schema = mongoose.schema({
     fname: String,
     lname: String,
-    username: String,
+    username: {type: String, unique: true},
     email: {type: String, unique: true},
     password: String
 })
