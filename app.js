74344aeca78f727429e8657eec7097224c99882b
Tim3Ds <Tim3Ds@gmail.com>
2017-02-16 10:46:04 -0700

    chat controler cmt out /user

--- app.js
+++ app.js
@@ -37,12 +37,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+//app.use('/users', users);
 
 // add reference to login and register website paths and what controller var from above
 app.use('/login', users);
 app.use('/register', users);
-app.use('/chat', users);
+app.use('/chat', chat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
