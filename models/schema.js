fe8b5e9f07fce96275ffbc37d5721f5a5e6eb45a
David Mariluch <davidmariluch@mail.weber.edu>
2017-02-18 15:17:25 -0700

    diddld with yo shema

--- models/schema.js
+++ models/schema.js
@@ -1,12 +1,22 @@
 var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
-var schema = mongoose.schema({
-    fname: String,
-    lname: String,
-    username: {type: String, unique: true},
-    email: {type: String, unique: true},
-    password: String
-});
+// var schema = mongoose.schema({
+//     fname: String,
+//     lname: String,
+//     username: {type: String, unique: true},
+//     email: {type: String, unique: true},
+//     password: String
+// });
 
-mongoose.model('ChatItUp', schema);
+module.exports.User = mongoose.model('User', new Schema({
+  id:           ObjectId,
+  fname:        { type: String, required: '{PATH} is required.' },
+  lname:        { type: String, required: '{PATH} is required.' },
+  username:     { type: String, required: '{PATH} is required.', unique: true },
+  email:        { type: String, required: '{PATH} is required.', unique: true },
+  password:     { type: String, required: '{PATH} is required.' },
+  data:         Object,
+}));
+
+//mongoose.model('ChatItUp', schema);
