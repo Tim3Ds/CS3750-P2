cc30b84f569464e4ff28ff9e368612101eb323c9
David Mariluch <davidmariluch@mail.weber.edu>
2017-02-18 00:00:00 -0700

    added the chat window and post stuff to test

--- app.js
+++ app.js
@@ -4,27 +4,38 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var mongoose = require('mongoose');
+
+// David: added for db validation
+var csrf = require('csurf');
+var session = require('client-sessions');
+var middleware = require('./middleware');
+
+
+// database dependencies
 mongoose.connect('mongodb://localhost/test');
 mongoose.Promise = Promise;
 
+// controllers
 var index = require('./controllers/index');
 var users = require('./controllers/users');
 var chat = require('./controllers/chat');
 
-
+// database connection
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
 });
 
+// express
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -34,6 +45,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  cookieName: 'session',
+  secret: 'keyboard cat',
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+}));
+app.use(csrf());
+app.use(middleware.simpleAuth);
+
+
+//routes
 app.use('/', index);
 app.use('/users', users);
 app.use('/chat', chat);
@@ -47,6 +72,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -58,6 +84,43 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// class methods
 
+/**
+ * Ensure a user is logged in before allowing them to continue their request.
+ *
+ * If a user isn't logged in, they'll be redirected back to the login page.
+ */
+requireLogin = function(req, res, next) {
+  if (!req.user) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+/**
+ * Given a user object:
+ *
+ *  - Store the user object as a req.user
+ *  - Make the user object available to templates as #{user}
+ *  - Set a session cookie with the user object
+ *
+ *  @ param {Object} req - The http request object.
+ *  @ param {Object} res - The http response object.
+ *  @ param {Object} user - A user object.
+ */
+createUserSession = function(req, res, user) {
+  var cleanUser = {
+    firstName:  user.firstName,
+    lastName:   user.lastName,
+    email:      user.email,
+    data:       user.data || {},
+  };
+
+  req.session.user = cleanUser;
+  req.user = cleanUser;
+  res.locals.user = cleanUser;
+};
 
 module.exports = app;
--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,9 @@
 var express = require('express');
+var bcrypt = require('bcryptjs');
+var schema = require('../models/schema');
+var app = require('../app');
 var router = express.Router();
 
-let validateLogin = (user, password) => {
-  
-}
-
-let afterUserSaveAttempt = (err, user) => {
-  
-}
 // /* GET users listing. */
 // router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
@@ -18,56 +14,76 @@ router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Chat It Up',
                            pageName: 'Register',
                            groupName: 'Project 2 : Group 3',
-                           user: 'user'
+                           user: 'user',
+                           csrfToken: req.csrfToken()
                          });
 });
-/* POST register from regisation form */
-// router.post('/register', function(req,res,next)
-// {
-//   var user = new User({
-//     fname = req.fname
-    
-//     // lname = req.body.lname,
-//     // username = req.body.username,
-//     // email = req.body.email,
-//     // password = req.body.password
-//   });
-//   console.log(user.fname);
-//   // user.save(function afterUserSaveAttempt(err, user){
-//   //   if (err)
-//   //   {
-//   //     if (err == 11000)
-//   //     {
-//   //       error = 'That email has already been used. Try another.'
-//   //     }
-//   //     return next(err); 
-      
-//   //   }
 
-//   //   res.render('register.pug', {error: error});
-//   // });
-//   res.redirect('/register');
-// });
+/* POST register from regisation form */
+/**
+ * Create a new user account.
+ *
+ * Once a user is logged in, they will be sent to the chat page.
+ */
+router.post('/register', function(req, res) {
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync(req.body.password, salt);
+
+  var user = new schema.User({
+    fmame:      req.body.fName,
+    lname:      req.body.lName,
+    username:   req.body.username,
+    email:      req.body.email,
+    password:   hash,
+  });
+  user.save(function(err, user) {
+    if (err) {
+      var error = 'Something bad happened! Please try again.';
+
+      if (err.code === 11000) {
+        error = 'That email is already taken, please try another.';
+      }
+
+      res.render('register', { error: error });
+    } else {
+      app.createUserSession(req, res, user);
+      res.redirect('window');
+    }
+  });
+});
 
 /* GET login page. */
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Chat It Up',
                         pageName: 'Login',
                         groupName: 'Project 2 : Group 3',
-                        user: 'user'
+                        user: 'user',
+                        csrfToken: req.csrfToken()
                       });
 });
+
 /* POST login request */
-router.post('/login', function(req, res, next) {
-  var user = new User;
-  user.findOne(function (err, user) {
-    if (err)
-    {
-      return next(err);
+/**
+ * Log a user into their account.
+ *
+ * Once a user is logged in, they will be sent to the chat page.
+ */
+router.post('/login', function(req, res) {
+  models.schema.findOne({ email: req.body.email }, 'fName lName username email password data', function(err, user) {
+    if (!schema) {
+      res.render('login', { error: "Incorrect email / password.", csrfToken: req.csrfToken() });
+    } else {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        app.createUserSession(req, res, user);
+        res.redirect('window');
+      } else {
+        res.render('login', { error: "Incorrect email / password.", csrfToken: req.csrfToken() });
+      }
     }
-    res.redirect('/chat');
   });
 });
+
+
 
 
 module.exports = router;
