58cedc51ea1a7d2d4685ba2f894350d99168f0d0
Tim3Ds <Tim3Ds@gmail.com>
2017-03-01 16:48:08 -0700

    add server messages login logout

--- models/socket.js
+++ models/socket.js
@@ -4,9 +4,19 @@ module.exports = (io) => {
     var router = app.Router();
     var userCount = 0;
     // socket.io events
-    io.on('connection', function (socket) {
+    io.on('connection', function (socket, req) {
         userCount++;
         console.log('a user connected ' + userCount + ' user(s)');
+        socket.emit('message',{
+            username: 'Chat It Up', 
+            text: 'Welcome to Chat', 
+            time: new Date().toLocaleTimeString().substring(0, 5) + ' ' + new Date().toLocaleTimeString().substring(9, 11)
+        });
+        io.emit('message', {
+            username: 'Chat It Up', 
+            text: req.user.username + 'has joined Chat', 
+            time: new Date().toLocaleTimeString().substring(0, 5) + ' ' + new Date().toLocaleTimeString().substring(9, 11)
+        });
         socket.on('send', function (msg) {
             var stamp = new Date().toLocaleTimeString();
             console.log('sending message: ' + 
@@ -23,6 +33,11 @@ module.exports = (io) => {
         socket.on('disconnect', function(){
             userCount--;
             console.log('user disconnected ' + userCount + ' user(s)');
+            io.emit('message',{
+                username: 'Chat It Up', 
+                text: req.user.username + 'has left Chat', 
+                time: new Date().toLocaleTimeString().substring(0, 5) + ' ' + new Date().toLocaleTimeString().substring(9, 11)
+            });
         });
     });
 
